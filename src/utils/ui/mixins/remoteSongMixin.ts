475c3dae55703b478d6efa48bd165912b7336f23
Sahil Gupte <sahilsachingupte@gmail.com>
2022-06-13 22:04:42 +0530

    Fetch artist cover images after adding to library
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- src/utils/ui/mixins/remoteSongMixin.ts
+++ src/utils/ui/mixins/remoteSongMixin.ts
@@ -8,11 +8,33 @@
  */
 
 import { Component, Vue } from 'vue-property-decorator'
+import { vxm } from '@/mainWindow/store'
 
 @Component
 export default class RemoteSong extends Vue {
-  public addSongsToLibrary(...songs: Song[]) {
-    window.DBUtils.storeSongs(songs)
+  public async addSongsToLibrary(...songs: Song[]) {
+    const storedSongs = await window.DBUtils.storeSongs(songs)
+    this.fetchCoverDetails(...storedSongs)
     this.$toasted.show(`Added ${songs.length} songs to library`)
+  }
+
+  private async fetchCoverDetails(...songs: (Song | undefined)[]) {
+    for (const s of songs) {
+      if (s && s.artists) {
+        for (const a of s.artists) {
+          if (!a.artist_coverPath) {
+            const fetchedArtist =
+              (await vxm.providers.spotifyProvider.getArtistDetails(a)) ??
+              (await vxm.providers.youtubeProvider.getArtistDetails(a))
+
+            console.log(fetchedArtist)
+            await window.DBUtils.updateArtist({
+              ...a,
+              artist_coverPath: fetchedArtist?.artist_coverPath
+            })
+          }
+        }
+      }
+    }
   }
 }
