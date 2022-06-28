91aed1594e4c2100ea8f70ed3ba1e4eb8fe0072e
Sahil Gupte <sahilsachingupte@gmail.com>
2022-06-28 17:31:02 +0530

    Chocolatey get api key from env file

--- chocolatey/pack.sh
+++ chocolatey/pack.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+. ./.env
+
+rm -f ./*.nupkg
 docker run --rm -v $PWD:$PWD -w $PWD linuturk/mono-choco pack moosync.nuspec
+docker run --rm -v $PWD:$PWD -w $PWD linuturk/mono-choco push *.nupkg --api-key $API_KEY
