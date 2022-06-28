275e125eb8c5739a54d186a09e729271164fc819
Sahil Gupte <sahilsachingupte@gmail.com>
2022-06-28 12:06:31 +0530

    [skip ci] Remove 32bit details from chocolatey package

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,19 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = ''
 $url64 = 'https://github.com/Moosync/Moosync/releases/download/v3.0.1/Moosync-3.0.1-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = $url
   url64bit       = $url64
 
   softwareName   = 'Moosync*'
 
-  checksum       = ''
-  checksumType   = 'sha256'
   checksum64     = 'eb1652b44d006f6615a1bcc999bdc936e6c6cbc9251db3cd76c248551f6f2508'
   checksumType64 = 'sha256'
 
