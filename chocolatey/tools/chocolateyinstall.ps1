8a638cbaf7b3d96fbf4e426906f7848dccc58f9b
Ovenoboyo <ovenoboyo@gmail.com>
2022-09-14 20:20:40 +0530

    Bump chocolatey
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Moosync/Moosync/releases/download/v5.0.0/Moosync-5.0.0-win-x64.exe'
+$url64 = 'https://github.com/Moosync/Moosync/releases/download/v5.0.1/Moosync-5.0.1-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName   = 'Moosync*'
 
-  checksum64     = '25770c6eaf8569492e86fb54cab23c682f389dec4e83ac3cbb5504f524e9de0b'
+  checksum64     = 'd64d203a82c891e2ceb0bc3a4ce7ed92ed9ebb4b56e7f624f0097c7d305ae49a'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
