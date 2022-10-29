5d4293d304e5c37571d7fb3bbfeec44d16e73efd
Ovenoboyo <ovenoboyo@gmail.com>
2022-10-29 17:37:39 +0530

    Fix exported types
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/types/declarations/exported.d.ts
+++ src/types/declarations/exported.d.ts
@@ -395,7 +395,7 @@ type ExtraExtensionEventReturnType<T extends ExtraExtensionEventTypes> =
       : void)
   | void
 
-type ExtraExtensionEventData<T extends ExtraExtensionEventTypes> = T extends 'requestedPlaylistSongs'
+type ExtraExtensionEventData<T extends ExtraExtensionEventTypes | unknown> = T extends 'requestedPlaylistSongs'
   ? [playlistID: string, invalidateCache: boolean, nextPageToken: unknown | undefined]
   : T extends 'requestedPlaylists'
   ? [invalidateCache: boolean]
