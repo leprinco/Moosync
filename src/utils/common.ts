e90c0073e0b21b2e283f275f2fd3b7da2c13f626
Ovenoboyo <ovenoboyo@gmail.com>
2022-11-18 15:09:46 +0530

    Default to len of array if n is greater than len
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -224,7 +224,7 @@ export function getRandomFromArray(arr: unknown[], n: number) {
   const result = new Array(n)
   let len = arr.length
   const taken = new Array(len)
-  if (n > len) throw new RangeError('getRandom: more elements taken than available')
+  n = Math.min(n, len - 1)
   while (n--) {
     const x = Math.floor(Math.random() * len)
     result[n] = arr[x in taken ? taken[x] : x]
