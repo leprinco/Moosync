35dab6bd14ef04155bf64170766a6c7d8eefa1d1
Ovenoboyo <ovenoboyo@gmail.com>
2023-01-01 20:46:03 +0530

    Update choco, aur and flatpak
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Moosync/Moosync/releases/download/v6.0.0/Moosync-6.0.0-win-x64.exe'
+$url64 = 'https://github.com/Moosync/Moosync/releases/download/v7.0.0/Moosync-7.0.0-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName   = 'Moosync*'
 
-  checksum64     = 'c222065f64fffcde9947dc08a81a2a857f9366b73b2c52396946cbbde6844880'
+  checksum64     = '42ffc93f774740a41ff7be4ca31008fb417a9e3d56519055ba9654123adbd7d3'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
