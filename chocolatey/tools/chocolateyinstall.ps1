a7479bf32e1a80b4c3063c0a4bd57044370b6c9a
Sahil Gupte <ovenoboyo@gmail.com>
2023-08-13 08:59:58 -0400

    Update packages
    Signed-off-by: Sahil Gupte <ovenoboyo@gmail.com>

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Moosync/Moosync/releases/download/v9.0.1/Moosync-9.0.1-win-x64.exe'
+$url64 = 'https://github.com/Moosync/Moosync/releases/download/v10.0.0/Moosync-10.0.0-win-x64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName   = 'Moosync*'
 
-  checksum64     = 'cf1b0cdb31fdc455ec2a440e3e87c4c98f26723f7e5b78489c326e57391e4a10'
+  checksum64     = '59073a8b650f5553fe63606298ad180d3d5ee8a787a72d46836112e2cf41b95c'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
